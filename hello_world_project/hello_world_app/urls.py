3b7ecd3fd4c60e9c735327c02e28f47d453a5a75
César <cesarlpb@gmail.com>
2023-01-11 12:10:06 +0100

    Añadimos 404 y main

--- hello_world_project/hello_world_app/urls.py
+++ hello_world_project/hello_world_app/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('hello_world/', views.hello_world, name='hello_world'),
     path('members/', views.all_members, name='members'),
     path('members/details/<int:member_id>/', views.member, name='member'),
+    path('testing/', views.testing, name='testing'), 
 ]
