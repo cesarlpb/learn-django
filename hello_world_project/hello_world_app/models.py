a7ac1334c83cd889c81ad78241510548e7fa9304
César <cesarlpb@gmail.com>
2023-01-11 10:03:02 +0100

    Migración 2 - añadimos phone y joined_date

--- hello_world_project/hello_world_app/models.py
+++ hello_world_project/hello_world_app/models.py
@@ -4,3 +4,5 @@ class Member(models.Model):
     id = models.IntegerField(primary_key=True)
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=20, null=True)
+    joined_date = models.DateField(null=True)
