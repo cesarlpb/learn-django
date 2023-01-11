b6dbeb7d53aea2946217537b72308cf6c32f45d5
César <cesarlpb@gmail.com>
2023-01-11 10:03:14 +0100

    Añadimos vista de todos los members

--- hello_world_project/hello_world_app/urls.py
+++ hello_world_project/hello_world_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('hello_world/', views.hello_world, name='hello_world'),
+    path('members/', views.all_members, name='members'),
 ]
--- hello_world_project/hello_world_app/views.py
+++ hello_world_project/hello_world_app/views.py
@@ -1,7 +1,15 @@
 from django.template import loader
 from django.http import HttpResponse
+from .models import Member
 
 def hello_world(request):
     template = loader.get_template('hello_world.html')
     return HttpResponse(template.render())
-  
+
+def all_members(request):
+  my_members = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'my_members': my_members,
+  }
+  return HttpResponse(template.render(context, request))
