b1d9ef096a2614521b94b2972266bc422c607e6a
César <cesarlpb@gmail.com>
2023-01-11 12:43:56 +0100

    Registramos list_display para Member

--- hello_world_project/hello_world_app/admin.py
+++ hello_world_project/hello_world_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Member
 
-admin.site.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "phone", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
