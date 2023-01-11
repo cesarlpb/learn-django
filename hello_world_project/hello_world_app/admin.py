bca22e3ab6cec3d7a9af5f3d340f7451c8cc95f8
César <cesarlpb@gmail.com>
2023-01-11 12:49:05 +0100

    Añadimos id

--- hello_world_project/hello_world_app/admin.py
+++ hello_world_project/hello_world_app/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Member
 
 class MemberAdmin(admin.ModelAdmin):
-  list_display = ("firstname", "lastname", "phone", "joined_date",)
+  list_display = ("id", "firstname", "lastname", "phone", "joined_date",)
   
 admin.site.register(Member, MemberAdmin)
