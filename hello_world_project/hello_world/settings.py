7b1a9cc350f73b881483cac7691fc75107acd4ce
César <cesarlpb@gmail.com>
2023-01-13 10:43:05 +0100

    Añadimos ruta para staticfiles -> assets globales

--- hello_world_project/hello_world/settings.py
+++ hello_world_project/hello_world/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
