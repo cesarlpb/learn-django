45cf949f8b8500b0236929e4b4458515ed28fda6
César <cesarlpb@gmail.com>
2023-01-16 09:51:29 +0100

    slug - Añadimos campo con contenido predeterminado a partir de firstname y lastname

--- hello_world_project/hello_world_app/admin.py
+++ hello_world_project/hello_world_app/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from .models import Member
 
 class MemberAdmin(admin.ModelAdmin):
-  list_display = ("id", "firstname", "lastname", "phone", "joined_date",)
   
+  list_display = ("id", "firstname", "lastname", "phone", "joined_date",)
+  prepopulated_fields = {"slug": ("firstname", "lastname")}
+
 admin.site.register(Member, MemberAdmin)
