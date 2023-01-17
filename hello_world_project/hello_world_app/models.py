42f601c129ae5b1dc92bac36a4f8425724205195
César <cesarlpb@gmail.com>
2023-01-17 10:03:13 +0100

    Añadimos fecha del día actual en joined_date

--- hello_world_project/hello_world_app/models.py
+++ hello_world_project/hello_world_app/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 import uuid 
+from datetime import datetime
 
 def generate_slug_hash():
     return str(uuid.uuid1())[:8]
+
+def today():
+    return datetime.today().date()
+
 
 class Member(models.Model):
     id = models.IntegerField(primary_key=True)
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
     phone = models.CharField(max_length=20, null=True)
-    joined_date = models.DateField(null=True)
+    joined_date = models.DateField(default=today)
     #slug_hash = models.UUIDField(default=uuid.uuid1, null=False)
     slug_hash = models.CharField(max_length=8, default=generate_slug_hash, null=False)
     slug = models.SlugField(default="", null=False)
