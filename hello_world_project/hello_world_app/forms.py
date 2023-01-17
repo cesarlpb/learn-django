68af7374e00f6227b70922135ab8f0d6417634e2
César <cesarlpb@gmail.com>
2023-01-17 10:04:00 +0100

    Separamos el MemberForm en dos forms -> Create y Update

--- hello_world_project/hello_world_app/forms.py
+++ hello_world_project/hello_world_app/forms.py
@@ -1,12 +1,22 @@
 from django import forms
 from .models import Member
 
-class MemberForm(forms.ModelForm):
+class CreateMemberForm(forms.ModelForm):
     class Meta:
         model = Member
-        fields = ['firstname', 'lastname', 'phone', 'joined_date']
+        fields = ['firstname', 'lastname', 'phone']
 
     def __init__(self, *args, **kwargs):
-        super(MemberForm, self).__init__(*args, **kwargs)
+        super(CreateMemberForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control my-2'
+
+class UpdateMemberForm(forms.ModelForm):
+    class Meta:
+        model = Member
+        fields = ['firstname', 'lastname', 'phone']
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateMemberForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control my-2'
