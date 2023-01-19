99819123b77a8b360e1bf9d407e51dcfdb22b145
César <cesarlpb@gmail.com>
2023-01-19 12:59:18 +0100

    Parte 5 - tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
@@ -11,11 +12,16 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Retornamos las 5 últimas preguntas publicadas en orden DESC -> -"""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
