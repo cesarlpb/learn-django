c17721f5be05528e1926ef53f96f04f85c458d82
César <cesarlpb@gmail.com>
2023-01-19 11:43:16 +0100

    Parte 4 usando vistas genéricas

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,15 +4,8 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-        # app "polls" -> pregunta 5
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-        # app "polls" -> pregunta 5 -> resultados de pregunta 5
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /polls/5/vote/
-        # app "polls" -> pregunta 5 -> votar pregunta 5
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
