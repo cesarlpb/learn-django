1a4adf411369532ec70621e92b4548f8f38d9ae0
César <cesarlpb@gmail.com>
2023-01-20 12:21:46 +0100

    Intro - Ejemplo subclase. Animal -> Perro y Gato

--- class_views/class_views/urls.py
+++ class_views/class_views/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
 
-from . import views
+#from . import views # importa todas las vistas y hay que navegar como "views.MyView.as_view()"
+#from class_views.views import MyView # importa solo la vista que necesitamos y navegamos como "MyView.as_view()"
+from class_views.views import SubClaseView
+
+# Ejemplo con animales y perros y gatos
+from class_views.views import Perro, Gato
 
 app_name = 'class_views'
 urlpatterns = [
-    path('', views.MyView.as_view(), name='index')
+    path('', SubClaseView.as_view(), name='index'),
+    path('animal/perro/', Perro.as_view(), name='perro'),
+    path('animal/gato/', Gato.as_view(), name='gato')
 ]
 """
  path('', views.IndexView.as_view(), name='index')
--- class_views/class_views/views.py
+++ class_views/class_views/views.py
@@ -1,7 +1,31 @@
 from django.http import HttpResponse
 from django.views import View
 
+variable_global = "Esto es un string global"
+
 class MyView(View):
+    necesita_self = "Esto es un string en la clase"
+    variable_en_clase = "Este contenido no saldrá en la respuesta"
     def get(self, request):
-        result = "Esto es un string"
-        return HttpResponse(result)
+        no_necesita_self = "Esto es un string en el método"
+        return HttpResponse(f"{variable_global}. {self.necesita_self}. {no_necesita_self}. {self.variable_en_clase}.")
+
+class SubClaseView(MyView):
+    variable_en_clase = "String desde subclase"
+    def get(self, request):
+        return HttpResponse(f"Hola desde subclase.")
+    
+# Ejemplo con animales y perros y gatos
+class Animal(View):
+    animal = "Animal"
+    sonido = "Sonido del animal"
+    def get(self, request):
+        return HttpResponse(f"{self.animal}: {self.sonido}")
+class Perro(Animal):
+    animal = "Perro"
+    sonido = "Guau"
+class Gato(Animal):
+    animal = "Gato"
+    sonido = "Miau"
+        
+        
