f125c12682746c9e8475a14f466ee2db7859d2e6
César <cesarlpb@gmail.com>
2023-01-20 16:51:00 +0100

    Generic - Creamos modelos Publisher, Author, Book

--- class_views/class_views/admin.py
+++ class_views/class_views/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Animal
+from .models import Animal, Publisher, Author, Book
 
 admin.site.register(Animal)
+admin.site.register(Publisher)
+admin.site.register(Author)
+admin.site.register(Book)
--- class_views/class_views/models.py
+++ class_views/class_views/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+# Ejemplo con animales 
 class Animal(models.Model):
     name = models.CharField("Nombre", max_length=50)
     species = models.CharField("Especie", max_length=50)
@@ -7,3 +8,37 @@ class Animal(models.Model):
 
     def __str__(self):
         return f"{self.species}: {self.name}"
+
+# url https://docs.djangoproject.com/en/4.1/topics/class-based-views/generic-display/
+# Ejemplo con Editorial, Autor y Libro
+class Publisher(models.Model):
+    name = models.CharField("Nombre", max_length=30)
+    address = models.CharField("Dirección", max_length=50)
+    city = models.CharField("Ciudad", max_length=60)
+    state_province = models.CharField("Provincia", max_length=30)
+    country = models.CharField("País", max_length=50)
+    website = models.URLField("Sitio web")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    salutation = models.CharField("Sr/Sra", max_length=10)
+    name = models.CharField("Nombre", max_length=200)
+    email = models.EmailField("Email")
+    headshot = models.ImageField("Foto", upload_to='author_headshots')
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField("Título", max_length=200)
+    authors = models.ManyToManyField('Author')
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    publication_date = models.DateField("Fecha de publicación")
+
+    def __str__(self):
+        return f"{self.title}"
