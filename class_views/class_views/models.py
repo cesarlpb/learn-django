38aa56a0f0865b89b9eb9f44d9d156de3c48d76d
César <cesarlpb@gmail.com>
2023-01-20 12:52:13 +0100

    Intro - Migración de modelo Animal

--- class_views/class_views/models.py
+++ class_views/class_views/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Animal(models.Model):
+    name = models.CharField("Nombre", max_length=50)
+    species = models.CharField("Especie", max_length=50)
+    sound = models.CharField("Sonido", max_length=50)
+
+    def __str__(self):
+        return f"{self.species}: {self.name}"
