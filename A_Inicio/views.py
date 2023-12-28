0581b3ac9783f8eed511af1994459b15a852c65a
César <cesarlpb@gmail.com>
2023-12-28 11:48:17 +0100

    Login con mensaje sin validación de user

--- A_Inicio/views.py
+++ A_Inicio/views.py
@@ -5,4 +5,13 @@ def index(request):
     return render(request, "index.html")
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "GET":
+        print("Me han pedido GET")
+        return render(request, "login.html")
+    elif request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        print("Me han pedido POST")
+        print(username, password)
+        mensaje = "Login recibido. Hola " + username  + "!"
+        return render(request, "index.html", context={"mensaje": mensaje})
