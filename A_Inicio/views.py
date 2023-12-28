ef2cbcce7535717580bcad2a4619de12a75510f8
César <cesarlpb@gmail.com>
2023-12-28 12:14:38 +0100

    Formulario de login con mensajes success y error

--- A_Inicio/views.py
+++ A_Inicio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
 def index(request):
     # Llamar a db, etc...
@@ -6,12 +8,22 @@ def index(request):
 
 def login(request):
     if request.method == "GET":
-        print("Me han pedido GET")
         return render(request, "login.html")
     elif request.method == "POST":
         username = request.POST.get("username")
         password = request.POST.get("password")
-        print("Me han pedido POST")
-        print(username, password)
-        mensaje = "Login recibido. Hola " + username  + "!"
-        return render(request, "index.html", context={"mensaje": mensaje})
+        
+        user = authenticate(request, username=username, password=password)
+
+        # print("Me han pedido POST")
+        # print(username, password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, 'Login exitoso. Bienvenido, ' + username)
+            return redirect('index')
+        else:
+            messages.error(request, 'Credenciales inválidas. Por favor, inténtalo de nuevo.')
+            return redirect('login')
+        
+        
