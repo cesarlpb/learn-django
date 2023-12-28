abc6a1ac566b5908d7908c7dd00c7d4219a39deb
César <cesarlpb@gmail.com>
2023-12-28 11:17:34 +0100

    Página de login

--- A_Inicio/urls.py
+++ A_Inicio/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from A_Inicio.views import index
+from A_Inicio.views import index, login
 
 urlpatterns = [
     path("", index, name="index"),
+    path("login/", login, name="login"),
 ]
--- A_Inicio/views.py
+++ A_Inicio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 def index(request):
     # Llamar a db, etc...
     return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
